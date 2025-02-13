6f417a91d9917430d03c7c465cf0506f85a07760
Max Ayres <maxayres1234@gmail.com>
2025-02-13 14:13:35 +0000

    Stuff

---
+++ scripts/teacher.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded0", function()
+{
+const classOneBtn = document.getElementById('classOneButton');
+classOneBtn.addEventListener("click", function(event){
+classOne.classList.toggle('classHidden')
+
+});
+
+
+}); 
