458076f21f501f315574331eb0e9ba5cbb8c3f31
Max Ayres <maxayres1234@gmail.com>
2025-02-17 16:27:48 +0000

    Clicking the class content buttons will now display the correct information.

--- scripts/teacher.js
+++ scripts/teacher.js
@@ -1,39 +1,56 @@
 document.addEventListener("DOMContentLoaded", function()
 {
 
-/* --- Button and code for showing class one information --- */
+/* --- Assigning constant variables based on HTML IDs --- */
 const classOneBtn = document.getElementById('classOneButton');
 const classTwoBtn = document.getElementById('classTwoButton');
 const classThreeBtn = document.getElementById('classThreeButton');
 const element = document.getElementById('classDetails');
 const classOneContent = document.getElementById('classOne');
 const classTwoContent = document.getElementById('classTwo');
+const classThreeContent = document.getElementById('classThree');
+
+const addClassBtn = document.getElementById('addClassButton');
 
 classOneBtn.addEventListener("click", function()
 {
     divClear();
-    //classToggle(classOneContent);
+    classToggle(classOneContent);
 });
 
 classTwoBtn.addEventListener("click", function()
 {
+    divClear();
     classToggle(classTwoContent);
 });
 
+classThreeBtn.addEventListener("click", function()
+{
+    divClear();
+    classToggle(classThreeContent);
+});
+
+addClassBtn.addEventListener("click", function()
+{
+    
+});
+
+/* --- Clears the currently displayed content. --- */ 
 function divClear()
 {
-    element.innerHTML = "Hello";
+    element.innerHTML = "";
+    console.log("Content has been cleared");
 }
 
+/* --- Changes the hiddenClass property for the clicked button --- */
 function classToggle(chosenClass)
 {
-   /* const temp = chosenClass; //stored as temp. 
-    chosenClass.innerHTML = "";
-    chosenClass.classList.toggle('hiddenClass');
-    temp.classList.toggle('hiddenClass');*/
-    
-    
-
+    console.log("Begin classToggle method");
+    let temp = chosenClass.cloneNode(true);
+    temp.classList.remove('hiddenClass');
+    element.appendChild(temp);
+    console.log("New content should now be displayed.");
+   
    /* if (!chosenClass.innerHTML)
     {
         alert("Something");
