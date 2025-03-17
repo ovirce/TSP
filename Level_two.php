33469626bf3912b64ff34f4db5a16e6de3abc585
saFoolady <112252@student.lincolncollege.ac.uk>
2025-03-17 14:33:20 +0000

    Add files via upload

--- Level_two.php
+++ Level_two.php
@@ -1,19 +1,21 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-<title><h1>16th Century Quest </h1></title>
+<header>
+
+  <body>
+  <title><h1>16th Century Quest </h1></title>
     <meta charset="UTF-8">
+    <p><a href="logout.php">Log out</a></p>
+    <p><a href="Level_one.php">Level one</a></p>
+    <p><a href="Level_three.php">Level three</a></p>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Form</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
     <script src="scripts/index.js"></script>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    </body>
-</body>
-</html>
+    <h2>Level two</h2>
+    
+    <img src="" alt=" ">
+    <img scr=" " alt="">
+  </body>
+</header>
