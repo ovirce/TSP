bd538398317ce781ce1eb5cd215ed30dcd2904fc
saFoolady <112252@student.lincolncollege.ac.uk>
2025-03-20 12:03:16 +0000

    Add files via upload

--- Level_three.php
+++ Level_three.php
@@ -10,8 +10,11 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
     <script src="scripts/index.js"></script>
-    <h2>Level Three</h2>
-    <p><a href="logout.php">Log out</a></p>
+    <h2>Level three</h2>
+    <a href="logout.php">Log out</a>
+    <a href="logout.php">Log out</a>&nbsp;
+    <a href="Level_one.php"> Level one </a>&nbsp;
+    <a href="Level_two.php">Level two</a></p>
     <img src="" alt=" ">
     <img scr=" " alt="">
   </body>
