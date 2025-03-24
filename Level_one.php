49611446f81bae5d24237b8fe05e19ccab7f840b
saFoolady <112252@student.lincolncollege.ac.uk>
2025-03-24 13:43:59 +0000

    Add files via upload

--- Level_one.php
+++ Level_one.php
@@ -1,21 +1,47 @@
 <!DOCTYPE html>
 <header>
 
-  <body>
-  <title><h1>16th Century Quest </h1></title>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Form</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
     <script src="scripts/index.js"></script>
+    
+  <body>
+
+  <title><h1>16th Century Quest </h1></title>
+   
+    <title>Login Form</title>
     <h2>Level one</h2>
-    <a href="index.php">Log out</a>&nbsp;
-    <a href="Level_two.php"> Level two </a>&nbsp;
-    <a href="Level_three.php">Level three</a></p>
-    <p> Who was the first Tudor monarch of England?</p>
-    <img src="" alt=" ">
-    <img scr=" " alt="">
+    <a href="index.php">Log out</a></p>
+    <a href="Level_two.php"> Next Level </a></p>
+    <a href="index.php">Quit</a></p>
+    <a href="studentPage.php"> Return to student page</a></p>
+    <p> What type of fabric did the wealthy people in the 16th century often wear?</p>
+    
+    <div class="wild-imgs-row">
+<div class="wild-imgs">
+  <img src="./images/Wool.png" alt="woolfabric" class="wild-pics">
+  <div class="crossover">
+    <div class="wild-text">The Dartmoor ponies are native to Devon. They are an endangered breed.</div>
+  </div>
+</div>
+ 
+<div class="wild-imgs">
+  <img src="./images/Velvet.png" alt="puffins" class="wild-pics">
+  <div class="crossover">
+    <div class="wild-text">Puffins can be found on the northeast of England, the North of Scotland and on the coast of Pembrokeshire in Wales.</div>
+  </div>
+</div>
+ 
+<div class="wild-imgs">
+  <img src="./images/Denim.png" alt="beaver" class="wild-pics">
+  <div class="crossover">
+    <div class="wild-text">Beavers can be found in their natural habitats in places such as Kent, Knapdale and Tay, and have been re-introduced to other areas in the UK.</div>
+  </div>
+</div>
+</div>
+ 
   </body>
 </header>
