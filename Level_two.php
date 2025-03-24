c00f9b4979e67c41b17f6ddc4a598b6b675b02e0
saFoolady <112252@student.lincolncollege.ac.uk>
2025-03-24 09:35:44 +0000

    Add files via upload

--- Level_two.php
+++ Level_two.php
@@ -4,8 +4,9 @@
   <body>
   <title><h1>16th Century Quest </h1></title>
     <meta charset="UTF-8">
-    <p><a href="logout.php">Log out</a></p>
-    <p><a href="Level_one.php">Level one</a></p>
+     <h2>Level two</h2>
+    <p><a href="logout.php">Log out</a>&nbsp;
+    <p><a href="Level_one.php">Level one</a>&nbsp;
     <p><a href="Level_three.php">Level three</a></p>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,7 +14,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
     <script src="scripts/index.js"></script>
-    <h2>Level two</h2>
+   
     
     <img src="" alt=" ">
     <img scr=" " alt="">
