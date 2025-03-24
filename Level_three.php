663cb11e003b212705f7e49b0f9c71977495ddf8
saFoolady <112252@student.lincolncollege.ac.uk>
2025-03-24 13:44:19 +0000

    Add files via upload

--- Level_three.php
+++ Level_three.php
@@ -1,21 +1,44 @@
 <!DOCTYPE html>
 <header>
-
-  <body>
-  <title><h1>16th Century Quest </h1></title>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Form</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
     <script src="scripts/index.js"></script>
+
+  <body>
+
+  <title><h1>16th Century Quest </h1></title>
+   
     <h2>Level three</h2>
-    <a href="index.php">Log out</a>&nbsp;
-    <a href="Level_one.php"> Next Level</a>&nbsp;
-    <a href="Level_two.php">Next Level</a></p>
-    <p> What type of clothing did the wealthy people in the 16th century often wear?</p>
-    <img src="questions.shakespeare.jpg" alt="">
-    <img src="questions.henry8.jpg" alt="">
+
+    <div class="wild-imgs-row">
+<div class="wild-imgs">
+  <img src="./images/mary.jpg" alt="Bloody Mary" class="wild-pics">
+  <div class="crossover">
+    <div class="wild-text">Correct! Mary Tudor/Mary I was the daughter of Henry the 8th and Catherine of Aragon.</div>
+  </div>
+</div>
+ 
+<div class="wild-imgs">
+  <img src="./images/anne.jpg" alt="Anne of Cleeves" class="wild-pics">
+  <div class="crossover">
+    <div class="wild-text">Incorrect, This is Anne of Cleeves, Henry the 8th 4th Wife.</div>.</div>
+  </div>
+</div>
+ 
+<div class="wild-imgs">
+  <img src="./images/elizabeth.jpg" alt="Queen Elizabeth" class="wild-pics">
+  <div class="crossover">
+    <div class="wild-text">Incorrect, this is Queen Elizabeth, the daughter of Henry the 8th and his second wife Anne Boleyn. She had a 44 year reign between 1558 - 1608.</div>
+  </div>
+</div>
+</div>
+ 
+    <a href="index.php">Log out</a></p>
+    <a href="index.php"> Quit</a></p>
+    <a href="studentPage.php"> Return to student page</a></p>
+   
   </body>
 </header>
