67e6a64afe6279b1e235c8aa8a41bb383af3c998
saFoolady <112252@student.lincolncollege.ac.uk>
2025-03-24 10:08:25 +0000

    Add files via upload

--- Level_one.php
+++ Level_one.php
@@ -10,10 +10,11 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
     <script src="scripts/index.js"></script>
-    <h2>Game Page</h2>
-    <a href="logout.php">Log out</a>&nbsp;
-    <a href="Level_one.php"> Level two </a>&nbsp;
-    <a href="Level_two.php">Level three</a>
+    <h2>Level one</h2>
+    <a href="index.php">Log out</a>&nbsp;
+    <a href="Level_two.php"> Level two </a>&nbsp;
+    <a href="Level_three.php">Level three</a></p>
+    <p> Who was the first Tudor monarch of England?</p>
     <img src="" alt=" ">
     <img scr=" " alt="">
   </body>
