b8ccb7b5c4b445fc0be01158ed54ebfbb019948b
saFoolady <112252@student.lincolncollege.ac.uk>
2025-03-24 13:44:38 +0000

    Add files via upload

---
+++ Level_two.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<header>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <script src="scripts/index.js"></script>
+  <body>
+    
+  <title><h1>16th Century Quest </h1></title>
+    
+     <h2>Level two</h2>
+
+
+     <div class="wild-imgs-row">
+<div class="wild-imgs">
+  <img src="martin-luther.jpg" alt="Martin Luther" class="wild-pics">
+  <div class="crossover">
+    <div class="wild-text">Incorrect, this is Martin Luther who started the Protestant Reformation in 1517.</div>
+  </div>
+</div>
+ 
+<div class="wild-imgs">
+  <img src="./images/henry8.jpg" alt="Henry The 8th" class="wild-pics">
+  <div class="crossover">
+    <div class="wild-text">Incorrect, This is Tudor King Henry the 8th.</div>
+  </div>
+</div>
+ 
+<div class="wild-imgs">
+  <img src="./images/shakespeare.jpg" alt="William Shakespeare" class="wild-pics">
+  <div class="crossover">
+    <div class="wild-text">Correct! William Shakespeare wrote famous plays such as Romeo and Juliet, Hamlet and The Tempest..</div>
+  </div>
+</div>
+</div>
+ 
+    <a href="index.php">Log out</a></p>
+    <a href="Level_one.php">Quit</a></p>
+    <a href="Level_three.php">Next Level</a></p>
+    <a href="studentPage.php"> Return to student page</a></p>
+    
+  </body>
+</header>
