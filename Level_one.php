da2ec7068b5c237cee52a21aba7c1eedafa6dc65
saFoolady <112252@student.lincolncollege.ac.uk>
2025-03-28 13:56:26 +0000

    Add files via upload

---
+++ Level_one.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<header>
+
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <script src="scripts/index.js"></script>
+    
+  <body>
+
+  <title><h1>16th Century Quest </h1></title>
+   
+    <title>Login Form</title>
+    <h2>Level one</h2>
+    <p> What type of fabric did the wealthy people in the 16th century often wear?</p>
+    
+    
+    <div class="wild-imgs-row">
+<button class="wild-imgs" onclick="handleClick('Wool')">
+  <img src="./images/Wool.png" alt="woolfabric" class="wild-pics">
+  <div class="crossover">
+    <div class="wild-text">Incorrect, wool was also worn and it was much more affordable in the 16th century than in the 21th, which made it a common item for peasants to wear as well.</div>
+  </div>
+</button> 
+
+ 
+<button class="wild-imgs" onclick="handleClick('Wool')">
+  <img src="./images/Velvet.png" alt="puffins" class="wild-pics">
+  <div class="crossover">
+    <div class="wild-text">Correct. Tudor Kings and Queens wore the most expennsive fabric such as silk, valvet and satin.</div>
+  </div>
+</botton> 
+</div>
+ 
+<button class="wild-imgs" onclick="handleClick('Wool')">
+  <img src="./images/Denim.png" alt="beaver" class="wild-pics">
+  <div class="crossover">
+    <div class="wild-text">Incorrect, denim fabric originated in the 17th century in France.</div>
+  </div>
+</button> 
+</div>
+
+
+<a href="logout.php">Log out</a></p>
+    <a href="Level_two.php"> Next Level </a></p>
+    <a href="index.php">Quit</a></p>
+    <a href="studentPage.php"> Return to student page</a></p>
+    </div>
+  </body>
+</header>
